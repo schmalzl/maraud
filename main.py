a7760072fedf28d0331d69d81f4e77de050c91e9
kian-schmalzl <kian.schmalzl@icloud.com>
2025-09-18 20:33:07 +0200

    add: main.py file

---
+++ main.py
@@ -0,0 +1,12 @@
+# maraud, v0.0.1.dev-alpha WIP
+# (main code and documentation)
+
+# Copyright (c) 2025 Kian Schmalzl
+import sys
+sys.dont_write_bytecode = True
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
